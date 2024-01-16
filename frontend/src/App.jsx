902e322acf94fe093a4f28cdf3ee3903fdc7948a
petersonguo <petersonguo@gmail.com>
2024-01-16 10:24:49 -0500

    Add New Button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,24 @@ import './pages/css/App.css';
 import Dashboard from "./pages/Dashboard";
 import StudyPage from "./pages/StudyPage";
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {FaPlus} from "react-icons/fa6";
 
 function App() {
     return (
         <div className={'h-screen overflow-hidden relative flex flex-row'}>
             <Router>
-                <div className={"py-4 px-8 border-gray-500 border-r-[2px] overflow-y-auto"}> {/*Create Sidebar here*/}
-                    <Link to={'/'}><h1>anji</h1></Link>
-                    {/*<div*/}
-                    {/*    className={'bg-[#d9d9d9] py-3 px-5 inline-flex flex-row items-center space-x-2 font-bold hover:cursor-pointer'}*/}
-                    {/*    onClick={() => {*/}
+                <div className={"w-[15vw] py-4 px-8 border-gray-500 border-r-[2px] overflow-y-auto"}> {/*Create Sidebar here*/}
+                    <Link to={'/'}>
+                        <h1>anji</h1>
+                    </Link>
+                    <div
+                        className={'text-white bg-[#C4554D] py-3 px-5 inline-flex flex-row items-center space-x-2 font-bold hover:cursor-pointer rounded-xl'}
+                        onClick={() => {
 
-                    {/*    }}>*/}
-                    {/*    <FaPlus/>*/}
-                    {/*    <p>New</p>*/}
-                    {/*</div>*/}
+                        }}>
+                        <FaPlus/>
+                        <p>New</p>
+                    </div>
                 </div>
                 <Routes>
                     <Route path={'/'} exact element={<Dashboard/>}/>
