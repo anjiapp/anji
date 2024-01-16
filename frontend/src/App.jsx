eef2e7d9e0b6077430bd553d84576eb8819bc983
petersonguo <petersonguo@gmail.com>
2024-01-16 00:41:56 -0500

    Update left bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
 import './pages/css/App.css';
 import Dashboard from "./pages/Dashboard";
 import StudyPage from "./pages/StudyPage";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
     return (
         <div className={'h-screen overflow-hidden relative flex flex-row'}>
-            <div className={"py-4 px-8 border-gray-500 border-r-[2px] overflow-y-auto"}> {/*Create Sidebar here*/}
-                <h1>anji</h1>
-                {/*<div*/}
-                {/*    className={'bg-[#d9d9d9] py-3 px-5 inline-flex flex-row items-center space-x-2 font-bold hover:cursor-pointer'}*/}
-                {/*    onClick={() => {*/}
-
-                {/*    }}>*/}
-                {/*    <FaPlus/>*/}
-                {/*    <p>New</p>*/}
-                {/*</div>*/}
-            </div>
             <Router>
+                <div className={"py-4 px-8 border-gray-500 border-r-[2px] overflow-y-auto"}> {/*Create Sidebar here*/}
+                    <Link to={'/'}><h1>anji</h1></Link>
+                    {/*<div*/}
+                    {/*    className={'bg-[#d9d9d9] py-3 px-5 inline-flex flex-row items-center space-x-2 font-bold hover:cursor-pointer'}*/}
+                    {/*    onClick={() => {*/}
+
+                    {/*    }}>*/}
+                    {/*    <FaPlus/>*/}
+                    {/*    <p>New</p>*/}
+                    {/*</div>*/}
+                </div>
                 <Routes>
                     <Route path={'/'} exact element={<Dashboard/>}/>
                     <Route path={'/deck'} element={<StudyPage/>}/>
