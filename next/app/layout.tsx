3d49516989f3a83741db8680b79dfa81062b273b
petersonguo <petersonguo@gmail.com>
2024-01-21 16:35:40 -0500

    Minor Side bar changes

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -2,9 +2,8 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
 import Link from "next/link";
-import {FaChevronDown, FaChevronRight, FaEllipsis, FaFolder, FaPlus} from "react-icons/fa6";
-
-const inter = Inter({subsets: ['latin']})
+import {FaPlus} from "react-icons/fa6";
+import SideNav from "@/components/SideNav";
 
 export const metadata: Metadata = {
     title: 'anji',
@@ -34,35 +33,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
                 <FaPlus/>
             </Link>
 
-            {/* convert this section to a sidenav client component or else updates won't work */}
-            <div className={'flex flex-col mt-6'}>
-                <div className={'bg-[#0000000D] py-1 px-3 inline-flex flex-row items-center space-x-2 font-bold rounded-l'}>
-                    <button><FaChevronDown className={'text-[x-small] font-black'}/></button>
-                    <FaFolder/>
-                    <p className={'truncate'} >Selected Folder</p>
-                    <span className={'flex-1 min-w-0'}></span>
-                    <FaEllipsis/>
-                </div>
-                <div className={'flex flex-col pl-7'}>
-                    <Link
-                        className={'truncate'}
-                        href={'#'}>
-                        Placeholder decks
-                    </Link>
-                    <Link
-                        className={'truncate'}
-                        href={'#'}>
-                        Longer Deck Name Longer Deck Name Longer Deck Name Longer Deck Name
-                    </Link>
-                </div>
-                <div className={'py-1 px-3 inline-flex flex-row items-center space-x-2 font-bold rounded-l'}>
-                    <FaChevronRight className={'text-[x-small] font-black'}/>
-                    <FaFolder/>
-                    <p className={'truncate'} >Tutorial</p>
-                    <span className={'flex-1 min-w-0'}></span>
-                    <FaEllipsis className={''}/>
-                </div>
-            </div>
+            <SideNav />
         </div>
         {
             children
--- next/components/SideNav.jsx
+++ next/components/SideNav.jsx
@@ -1,19 +1,37 @@
+'use client';
 import Link from "next/link";
-import {FaPlus} from "react-icons/fa6";
+import {FaChevronDown, FaChevronRight, FaEllipsis, FaFolder, FaPlus} from "react-icons/fa6";
 import React from "react";
 
 export default function SideNav() {
     return (
-        <nav className={"py-4 px-2 border-gray-500 border-r-[2px] overflow-y-auto resize-x"} id={'nav'}> {/*Create Sidebar here*/}
-            <Link href={'/dashboard'}>
-                <h1>anji</h1>
-            </Link>
-            <Link
-                className={'w-full text-white bg-[#C4554D] py-3 px-5 inline-flex flex-row items-center space-x-2 font-bold hover:cursor-pointer rounded-xl'}
-                href={'#'}>
-                <FaPlus/>
-                <p> New</p>
-            </Link>
+        <nav className={'flex flex-col mt-6'}>
+            <div className={'bg-[#0000000D] py-1 px-3 inline-flex flex-row items-center space-x-2 font-bold rounded-l'}>
+                <button><FaChevronDown className={'text-[x-small] font-black'}/></button>
+                <FaFolder/>
+                <p className={'truncate'}>Selected Folder</p>
+                <span className={'flex-1 min-w-0'}></span>
+                <FaEllipsis/>
+            </div>
+            <div className={'flex flex-col pl-7'}>
+                <Link
+                    className={'truncate'}
+                    href={'#'}>
+                    Placeholder decks
+                </Link>
+                <Link
+                    className={'truncate'}
+                    href={'#'}>
+                    Longer Deck Name Longer Deck Name Longer Deck Name Longer Deck Name
+                </Link>
+            </div>
+            <div className={'py-1 px-3 inline-flex flex-row items-center space-x-2 font-bold rounded-l'}>
+                <FaChevronRight className={'text-[x-small] font-black'}/>
+                <FaFolder/>
+                <p className={'truncate'}>Tutorial</p>
+                <span className={'flex-1 min-w-0'}></span>
+                <FaEllipsis className={''}/>
+            </div>
         </nav>
     )
 }
