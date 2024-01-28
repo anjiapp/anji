c436c32d8dca6fe7d82277ecab931aae40270801
petersonguo <petersonguo@gmail.com>
2024-01-28 14:37:52 -0500

    Rearrange algo folder to make api

--- backend/old/cardTest.js
+++
@@ -1,7 +0,0 @@
-function numberToDate(number) {
-    let d = new Date(number);
-    return d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
-}
-
-console.log(Date.now())
-console.log(numberToDate(Date.now()));
