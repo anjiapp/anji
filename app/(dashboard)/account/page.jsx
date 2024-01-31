c7fb68a424a207d5305b42c520a5f91d529bb0bf
petersonguo <petersonguo@gmail.com>
2024-01-31 14:05:46 -0500

    Remove redundancies

--- app/(dashboard)/account/page.jsx
+++ app/(dashboard)/account/page.jsx
@@ -1,7 +1,8 @@
-import {createClient} from '@/utils/supabase/client'
+import {createClient} from '@/utils/supabase/server'
 import AccountForm from '@/components/AccountForm'
+import {cookies} from 'next/headers'
 
-const supabase = createClient()
+const supabase = createClient(cookies())
 export default async function Account() {
     const {data: {user}} = await supabase.auth.getUser()
 
