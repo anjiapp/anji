af1da1270e4b0b109019ef438fa04ed5f4e92139
petersonguo <petersonguo@gmail.com>
2024-01-31 14:46:50 -0500

    Remove redundant stuff

--- app/api/src/main.js
+++
@@ -1,6 +0,0 @@
-import { R2 } from "./module/R2.js";
-import fs from "fs";
-const config = await JSON.parse(fs.readFileSync("config.json", "utf-8"));
-const R2Class = new R2(config);
-
-console.log(await R2Class.listData());
